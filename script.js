769ef0e211d33b49ebc180281dc861297346180d
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-11-20 13:59:08 -0500

    problem 3 completed

--- script.js
+++ script.js
@@ -45,3 +45,35 @@ for(let n = 5; n <= 10; n++){
 }
 
 // question 3
+// loop over CSV string
+// store each cell in the variable 
+// if ",", move to the next cell 
+//if "\n", move to the next row
+
+let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26"
+
+let cell1 =""
+let cell2 = ""
+let cell3 = ""
+let cell4 = ""
+let currentCell = 1
+
+for (let i = 0; i < csvData.length; i++) {
+    if (csvData[i] === ",") {
+        currentCell++ 
+    } else if (csvData[i] === "\n") {
+        console.log(cell1,cell2,cell3,cell4);
+        cell1 = cell2 = cell3 = cell4 = ""
+        currentCell = 1
+    } else {
+        if (currentCell === 1) {
+            cell1 += csvData[i]
+        } else if (currentCell === 2) {
+            cell2 += csvData[i]
+        } else if (currentCell === 3) {
+            cell3 += csvData[i]
+        } else if (currentCell === 4) {
+            cell4 += csvData[i]
+        } 
+    }
+}
